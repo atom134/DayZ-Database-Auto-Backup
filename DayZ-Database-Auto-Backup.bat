2ad1fe0e00da425ca230e7f766bfbd82bee0b38b
XxFri3ndlyxX <loll420@live.ca>
2018-09-24 21:36:37 -0400

    Create DayZ-Database-Auto-Backup.bat

---
+++ DayZ-Database-Auto-Backup.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+SET Dt=%DATE:~4,2%.%DATE:~7,2%.%DATE:~-4% %TIME:~0,2%.%TIME:~3,2%
+SET SrcFolder=C:\steamcmd\dayz_server\mpmissions\dayzOffline.chernarusplus\storage_1
+SET DestPath=C:\steamcmd\backups\
+SET TempFolder=C:\steamcmd\backups\temp\
+
+xcopy /O /X /E /H /K /Y "%SrcFolder%" "%TempFolder%"
+
+"C:\Program Files\7-Zip\7z" a "%DestPath%\DayZ_Database.%Dt%.7z" "%TempFolder%"
